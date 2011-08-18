54986dbe275d7b3b41559842606da995ec877fc9
Laurent Caillette <administrateur@Laptop-0.local>
2011-08-18 21:11:47 +0200

    Basic line parsing: only terms.

--- javascript/script.js
+++ javascript/script.js
@@ -11,26 +11,25 @@ var LANGUAGE_2 = "LANGUAGE_2" ;
 
 function parse( text ) {
   // http://regexpal.com
-  // http://regexpal.com/?flags=s&regex=(%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn(%20%2B%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn(%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn(%20%2B%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn&input=Fo%0AB%20ar%0A%0AWhat%0A%20ever%0A%0AFoo%0ABar%0A%20Foo%0ABar%0A%0AFoo%0A%20%20Bar%0AFoo%20%0A%20%20Bar%0A%0AFoo%0A%20%20Bar%0AFoo%0A%20%20%0A%0A
+  // http://regexpal.com/?flags=m&regex=%5Cn(%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn(%3F%3A(%3F%3A%20%2B%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn)*(%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn(%3F%3A(%3F%3A%20%2B%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn)*&input=%0AFo%0AB%20ar%0A%0AWhat%0A%20ever%0A%0AFoo%0ABar%0A%20Foo%0ABar%0A%0AFoo%0A%20Bar%0AFoo%0A%20Bar%0A%0AFoo%0A%20%20Bar%0A%20%20Bar%0AFoo%20%0A%20%20Bar%0A%0AFoo%0A%20%20Bar%0AFoo%0A%20%20%0A%0A
   characters = "0-9a-zA-Z" ;
-//  characters = ",;!\\?\\-\\wáéíóúÁÉÚÍÓÚőűŐŰöüÜÖœàâèêëïîôûçŒÀÂÈÊËÏÎÔÛÇ" ;
-//  termLine = "^([" + characters + "](?:[ " + characters + "])+)\n" ;
-//  definitionLine = "^ +([" + characters + "](?:[ " + characters + "])+)\n" ;
-//  var entry = new RegExp( termLine + definitionLine + "*" + termLine + definitionLine + "*" ) ;
-
-//  var entry = new RegExp(
-//      "([" + characters + "][ " + characters + "]*)\\n" +
-//      "( +[" + characters + "][ " + characters + "]*)"
-//  ) ;
-  var entry = new RegExp( "(line1)(line2)", "g" ) ;
+  characters = ",;!\\?\\-\\wáéíóúÁÉÚÍÓÚőűŐŰöüÜÖœàâèêëïîôûçŒÀÂÈÊËÏÎÔÛÇ" ;
+  termLine = "(?:[" + characters + "][ " + characters + "]*)" ;
+  definitionLine = "(?: +[" + characters + "][ " + characters + "]*)" ;
+  var entry = new RegExp(
+        "\n"
+      + "(" + termLine + ")\n((?:" + definitionLine + "\n)*)"
+      + "(" + termLine + ")\n((?:" + definitionLine + "\n)*)"
+      ,"g"
+  ) ;
 
   var array = [] ;
   var guard = 0 ;
 
-  while( guard++ < 10 ) {
+  while( guard++ < 100 ) {
     var match = entry.exec( text ) ;
     if( ! match ) break ;
-    array.push( { LANGUAGE_1 : match[ 1 ], LANGUAGE_2 : match[ 2 ] } ) ;
+    array.push( { LANGUAGE_1 : match[ 1 ], LANGUAGE_2 : match[ 3 ] } ) ;
   }
   return array ;
 } 
