3c3e89525dad435f5261133041fb023f3a83df4e
Laurent Caillette <administrateur@Laptop-0.local>
2011-08-18 22:06:04 +0200

    Full entry parsing.

--- javascript/script.js
+++ javascript/script.js
@@ -8,18 +8,23 @@ var UNAVAILABLE = "unavailable" ;
 var LANGUAGE_1 = "LANGUAGE_1" ;
 var LANGUAGE_2 = "LANGUAGE_2" ;
 
+var characters = ",;!\\?\\-\\wáéíóúÁÉÚÍÓÚőűŐŰöüÜÖœàâèêëïîôûçŒÀÂÈÊËÏÎÔÛÇ" ;
+var textExp = "(?:[" + characters + "][ " + characters + "]*)" ;
+var termExp = textExp ;
+var definitionLineExp = "(?: +" + textExp + ")" ;
+var definitionLineCapturingExp = "(?: +(" + textExp + "))" ;
 
-function parse( text ) {
+// Entry: a pair of Words.
+// Word: what's in a non-indented line.
+// Definition: the indented lines relative to a Word.
+
+function parseEntries( text ) {
   // http://regexpal.com
   // http://regexpal.com/?flags=m&regex=%5Cn(%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn(%3F%3A(%3F%3A%20%2B%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn)*(%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn(%3F%3A(%3F%3A%20%2B%5B0-9a-zA-Z%5D%5B%200-9a-zA-Z%5D*)%5Cn)*&input=%0AFo%0AB%20ar%0A%0AWhat%0A%20ever%0A%0AFoo%0ABar%0A%20Foo%0ABar%0A%0AFoo%0A%20Bar%0AFoo%0A%20Bar%0A%0AFoo%0A%20%20Bar%0A%20%20Bar%0AFoo%20%0A%20%20Bar%0A%0AFoo%0A%20%20Bar%0AFoo%0A%20%20%0A%0A
-  characters = "0-9a-zA-Z" ;
-  characters = ",;!\\?\\-\\wáéíóúÁÉÚÍÓÚőűŐŰöüÜÖœàâèêëïîôûçŒÀÂÈÊËÏÎÔÛÇ" ;
-  termLine = "(?:[" + characters + "][ " + characters + "]*)" ;
-  definitionLine = "(?: +[" + characters + "][ " + characters + "]*)" ;
-  var entry = new RegExp(
+  var entryExp = new RegExp(
         "\n"
-      + "(" + termLine + ")\n((?:" + definitionLine + "\n)*)"
-      + "(" + termLine + ")\n((?:" + definitionLine + "\n)*)"
+      + "(" + termExp + ")\n((?:" + definitionLineExp + "\n)*)"
+      + "(" + termExp + ")\n((?:" + definitionLineExp + "\n)*)"
       ,"g"
   ) ;
 
@@ -27,12 +32,30 @@ function parse( text ) {
   var guard = 0 ;
 
   while( guard++ < 100 ) {
-    var match = entry.exec( text ) ;
+    var match = entryExp.exec( text ) ;
     if( ! match ) break ;
-    array.push( { LANGUAGE_1 : match[ 1 ], LANGUAGE_2 : match[ 3 ] } ) ;
+    var array1 = build( match[ 1 ], match[ 2 ] ) ;
+    var array2 = build( match[ 3 ], match[ 4 ] ) ;
+
+    array.push( { LANGUAGE_1 : array1, LANGUAGE_2 : array2 } ) ;
   }
   return array ;
-} 
+}
+
+
+function build( term, definitionLines ) {
+  var lineExp = new RegExp( "" + definitionLineCapturingExp + "\n" ,"g" ) ;
+  var array = [ term ] ;
+  var guard = 0 ;
+
+  while( guard++ < 100 ) {
+    var match = lineExp.exec( definitionLines ) ;
+    if( ! match ) break ;
+    array.push( match[ 1 ] ) ;
+  }
+  return array ;
+
+}
 
 
 
