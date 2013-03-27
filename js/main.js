23ee644f3932df783e2643a0a8acc5437f0c7b18
caillette <laurent.caillette@gmail.com>
2013-03-27 23:26:25 +0100

    Szotargep 2: basic feature detection.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+function checkBrowserFeatures() {
+  $( '#required-features' ).append( '<h3>Checking required features…</h3>' ) ;
+  var required = [ 'applicationcache', 'history', 'webworkers' ] ;
+
+  var allGood = true ;
+  for( var i in required ) {
+    var supported = eval( 'Modernizr.' + required[ i ] ) ;
+    $( '#required-features' ).append(
+        '<p>' + required[ i ] + ( supported ? ' ok' : ' <b>not supported</b>' )+ '</p>') ;
+    allGood = allGood && supported ;
+  }
+
+  return allGood ;
+}
