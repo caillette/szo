ea46b7a5f4450fe66b90a18ef6ec756f6bdd0fe1
caillette <laurent.caillette@gmail.com>
2013-03-28 10:14:40 +0100

    Feature detection enhancements.

--- js/main.js
+++ js/main.js
@@ -5,12 +5,56 @@ function checkBrowserFeatures() {
   var required = [ 'applicationcache', 'history', 'webworkers' ] ;
 
   var allGood = true ;
-  for( var i in required ) {
-    var supported = eval( 'Modernizr.' + required[ i ] ) ;
-    $( '#required-features' ).append(
-        '<p>' + required[ i ] + ( supported ? ' ok' : ' <b>not supported</b>' ) + '</p>' ) ;
-    allGood = allGood && supported ;
+
+  if( isChromeUsingFileOrigin() ) {
+    allGood = false ;
+    reportHtml( '<p>Google Chrome <b>doesn\'t support file://</b> properly</p>' ) ;
+  } else {
+    for( var i in required ) {
+      var supported = eval( 'Modernizr.' + required[ i ] ) ;
+      report( required[ i ], supported ) ;
+      allGood = allGood && supported ;
+    }
+
+    report( 'transferableobjects', detectTransferable() ) ; // Not mandatory.
   }
 
-  return allGood ;
+  // Future:
+  // - Browser display (small or large).
+  // ?
+  return { capable : allGood } ;
+
+
+  function report( feature, supported ) {
+    reportHtml( '<p>' + feature + ( supported ? ' ok' : ' <b>not supported</b>' ) + '</p>' ) ;
+  }
+
+  function reportHtml( featureMessageHtml ) {
+    $( '#required-features' ).append( featureMessageHtml ) ;
+  }
+
+
+  function isChromeUsingFileOrigin() {
+    return window.location.origin == 'file://'
+        && window.navigator.userAgent.indexOf( 'Chrome' ) >= 0 ;
+  }
+
+  function detectTransferable() {
+    try {
+      var worker = new Worker( 'js/worker.js' ) ;
+      // http://updates.html5rocks.com/2011/12/Transferable-Objects-Lightning-Fast
+      worker.postMessage = worker.webkitPostMessage || worker.postMessage ;
+
+      var ab = new ArrayBuffer( 1 ) ; // TODO: use magic value to tell real worker it's dummy.
+      worker.postMessage( ab, [ ab ] ) ;
+      if( ab.byteLength ) {
+        return false ;
+      } else {
+        return true ;
+      }
+    } catch( e ) {
+      return false ;
+    }
+  }
+
 }
