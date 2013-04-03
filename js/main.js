3fec53266fddabdc7f4488ff74eb9945d74c6c21
caillette <laurent.caillette@gmail.com>
2013-04-03 08:45:40 +0200

    Refactoring: new feature-detection.js for browser feature verification.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,4 @@
 
-
-function verifyBrowserFeatures( div ) {
-  reportHtml( '<h3>Verifying browser features...</h3>' ) ;
-  var required = [ 'applicationcache', 'history', 'webworkers' ] ;
-
-  var allGood = true ;
-
-  if( isChromeUsingFileOrigin() ) {
-    allGood = false ;
-    reportHtml( '<p>Google Chrome <b>doesn\'t support file://</b> properly</p>' ) ;
-  } else {
-    // Workers don't work on Chrome with file:// so we don't send wrong message.
-    for( var i  = 0 ; i < required.length ; i ++ ) {
-      var supported = eval( 'Modernizr.' + required[ i ] ) ;
-      report( required[ i ], supported ) ;
-      allGood = allGood && supported ;
-    }
-  }
-
-  // In the future:
-  // - Browser display (small or large).
-  return { capable : allGood } ;
-
-
-  function report( feature, supported ) {
-    reportHtml( '<p>' + feature + ( supported ? ' ok' : ' <b>not supported</b>' ) + '</p>' ) ;
-  }
-
-  function reportHtml( featureMessageHtml ) {
-    $( div ).append( featureMessageHtml ) ;
-  }
-
-  function isChromeUsingFileOrigin() {
-    return window.location.origin == 'file://'
-        && window.navigator.userAgent.indexOf( 'Chrome' ) >= 0 ;
-  }
-
-}
-
-
 function documentReady() {
 
   var capabilities = verifyBrowserFeatures( '#browser-features' ) ;
