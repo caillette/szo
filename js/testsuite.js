cb38696cd13ed74e595c3ace7b4c2a5df29ec173
caillette <laurent.caillette@gmail.com>
2013-04-03 09:23:04 +0200

    Refactoring Vocabulary testing for reusable Vocabulary instance.

--- js/testsuite.js
+++ js/testsuite.js
@@ -29,28 +29,38 @@ test( 'Closure plus prototype', function() {
 
 module( 'Vocabulary' ) ;
 
-test( 'Instantiate Vocabulary from predefined Cards', function() {
-
+// So we can create a fresh instance for each test.
+function vocabulary1() {
   // Resolve some test-specific chicken-and-egg problem.
-  var dummyPack = {
+  var fakePackA = {
     id : function() { return 1 ; },
     url : function() { return 'some://where' }
   } ;
 
-  var card1 = new Card( [ 'Question', 'And more' ], [ 'Választ' ], [ 'Jó' ], dummyPack, 1 ) ;
-  var card2 = new Card( [ 'rien' ], [ 'semmi' ], [ 'Rossz' ], dummyPack, 2 ) ;
-  var card3 = new Card( [ 'Sans étiquette' ], [ 'jegy nélkül' ], [], dummyPack, 3 ) ;
+  var card1 = new Card( [ 'Question', 'And more' ], [ 'Választ' ], [ 'Jó' ], fakePackA, 1 ) ;
+  var card2 = new Card( [ 'rien' ], [ 'semmi' ], [ 'Rossz' ], fakePackA, 2 ) ;
+  var card3 = new Card( [ 'Sans étiquette' ], [ 'jegy nélkül' ], [], fakePackA, 3 ) ;
   var packA = new Pack( 1, 'some://where', [ card1, card2, card3 ] ) ;
-  var vocabulary = new Vocabulary( [ packA ] ) ;
 
-  deepEqual( vocabulary.tags(), [ 'Jó', 'Rossz' ], 'Find Vocabulary Tags' ) ;
-  deepEqual( vocabulary.cards(), [ card1, card2, card3 ], 'All Cards' ) ;
-  deepEqual( vocabulary.cards( 'Jó' ), [ card1 ], 'Cards by tag' ) ;
-  deepEqual( vocabulary.cards( null ), [ card3 ], 'Untagged Cards' ) ;
+  return {
+    fakePackA : fakePackA,
+    card1 : card1,
+    card2 : card2,
+    card3 : card3,
+    packA : packA,
+    vocabulary : new Vocabulary( [ packA ] )
+  }
+}
 
-  equal( packA.toString(), 'Pack{3;some://where}', 'Pack\'s toString' ) ;
-  equal( card1.toString(), 'Card{1@some://where}', 'Card\'s toString' ) ;
-  equal( vocabulary.toString(), 'Vocabulary{1;3;2}', 'Vocabulary\'s toString' ) ;
+test( 'Instantiate Vocabulary from predefined Cards', function() {
+  var v = vocabulary1() ;
+  deepEqual( v.vocabulary.tags(), [ 'Jó', 'Rossz' ], 'Find Vocabulary Tags' ) ;
+  deepEqual( v.vocabulary.cards(), [ v.card1, v.card2, v.card3 ], 'All Cards' ) ;
+  deepEqual( v.vocabulary.cards( 'Jó' ), [ v.card1 ], 'Cards by tag' ) ;
+  deepEqual( v.vocabulary.cards( null ), [ v.card3 ], 'Untagged Cards' ) ;
 
+  equal( v.packA.toString(), 'Pack{3;some://where}', 'Pack\'s toString' ) ;
+  equal( v.card1.toString(), 'Card{1@some://where}', 'Card\'s toString' ) ;
+  equal( v.vocabulary.toString(), 'Vocabulary{1;3;2}', 'Vocabulary\'s toString' ) ;
 } ) ;
 
