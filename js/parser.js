9478b602a4ef18f837185909d326d92905521e1a
caillette <laurent.caillette@gmail.com>
2013-04-06 07:12:10 +0200

    Fixing Firefox problem which tried to read XML from plain text.

--- js/parser.js
+++ js/parser.js
@@ -49,17 +49,25 @@ var Parser = function() {
 }() ;
 
 Parser.createParser = function( grammarSourceUri, onCompletion ) {
-  $.get( grammarSourceUri, function( parserSource ) {
-    try {
-      window.console.debug( 'Successfully created parser from ' + grammarSourceUri ) ;
-      onCompletion( new Parser( parserSource, grammarSourceUri ) ) ;
-    } catch( e ) {
-      onCompletion( new Parser( null, grammarSourceUri ) ) ;
-    }
-  } )
+  $.get(
+      grammarSourceUri,
+      null,
+      function( parserSource ) {
+        try {
+          window.console.debug( 'Successfully created parser from ' + grammarSourceUri ) ;
+          onCompletion( new Parser( parserSource, grammarSourceUri ) ) ;
+        } catch( e ) {
+          onCompletion( new Parser( null, grammarSourceUri + ' ' + e ) ) ;
+        }
+      },
+      'text'
+  )
   .fail( function( jqXhr, textStatus, errorThrown ) {
+    grammarSourceUri += ': ' + textStatus + '\n' + errorThrown ;
     onCompletion( new Parser( null, grammarSourceUri ) ) ;
-    // JQuery already logs failed GET errors.
+    // JQuery already logs some failed GET errors, but forgets some corner-cases
+    // like unrecognized XML with Firefox.
+    window.console.error( 'Could not load ' + grammarSourceUri ) ;
   } ) ;
 }
 
