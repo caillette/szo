bdafc01aabd4d4501536eff107ac8752eeb5efb3
caillette <laurent.caillette@gmail.com>
2013-04-07 06:00:15 +0200

    Code beauty.

--- js/loader.js
+++ js/loader.js
@@ -1,22 +1,21 @@
 function loadResources( div, onSuccess, onFailure ) {
 
   Parser.createDefaultParsers(
-      function( parsers ) {
-        var parsersHealthy = true ;
-        for( p = 0 ; p < parsers.length ; p ++ ) {
-          if( parsers[ p ].problem() ) {
-            $( div ).append( '<p>' + parsers[ p ].problem() + '</p>')
-            parsersHealthy = false ;
-          }
+    function( parsers ) {
+      var parsersHealthy = true ;
+      for( p = 0 ; p < parsers.length ; p ++ ) {
+        if( parsers[ p ].problem() ) {
+          $( div ).append( '<p>' + parsers[ p ].problem() + '</p>')
+          parsersHealthy = false ;
         }
-
-        if( parsersHealthy ) {
-          onSuccess( /*TODO: Vocabulary object*/ ) ;
-        } else {
-          onFailure() ;
-        }
-
       }
+
+      if( parsersHealthy ) {
+        onSuccess( /*TODO: Vocabulary object*/ ) ;
+      } else {
+        onFailure() ;
+      }
+    }
   ) ;
 
 }
